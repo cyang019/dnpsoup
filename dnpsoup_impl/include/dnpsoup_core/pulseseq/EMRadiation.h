62bad429b17a62dca17c80e920f6c4c74bb23814
Chen Yang <yangcnju@gmail.com>
2020-03-19 15:11:43 -0400

    start refactoring cache EMR lookup by assigning unique ids

--- dnpsoup_impl/include/dnpsoup_core/pulseseq/EMRadiation.h
+++ dnpsoup_impl/include/dnpsoup_core/pulseseq/EMRadiation.h
@@ -2,15 +2,17 @@
 #define DNPSOUP_EMRADIATION_H
 
 #include <iostream>
+#include "dnpsoup_core/common.h"
 
 namespace dnpsoup {
   namespace pulseseq{
   struct EMRadiation {
-      EMRadiation() : freq(0.0), phase(0.0), offset(0.0) {}
+      EMRadiation() 
+        : freq(0.0), phase(0.0), offset(0.0), id_(-1) {}
       EMRadiation(double f, double p)
-        : freq(f), phase(p) {}
+        : freq(f), phase(p), id_(-1) {}
       EMRadiation(double f, double p, double offset)
-        : freq(f), phase(p), offset(offset) {}
+        : freq(f), phase(p), offset(offset), id_(-1) {}
       EMRadiation(const EMRadiation &) = default;
       EMRadiation(EMRadiation &&) noexcept = default;
       EMRadiation& operator=(const EMRadiation &) = default;
@@ -22,14 +24,31 @@ namespace dnpsoup {
       double freq;    ///< in Hz  (gamma B1)
       double phase;   ///< in rad (phase)
       double offset;  ///< in Hz  (carrier)
+
+      void setId(int value) { id_ = value; }
+      int getId() const { return id_; }
+    private:
+      int id_;   ///< to lookup in cache
     };  // class EMRadiation
 
     std::istream& operator>>(std::istream&, EMRadiation &);
     std::ostream& operator<<(std::ostream&, const EMRadiation &);
 
   } // namespace pulseseq
-  bool sameValue(const pulseseq::EMRadiation &emr1, 
-      const pulseseq::EMRadiation &emr2, double eps);
+  inline bool sameValue(const pulseseq::EMRadiation &emr1, const pulseseq::EMRadiation &emr2, double eps)
+  {
+    auto res = approxEqual<double>(emr1.freq, emr2.freq, eps, eps) 
+      && approxEqual<double>(emr1.offset, emr2.offset, eps, eps)
+      && approxEqual<double>(emr1.phase, emr2.phase, eps, eps);
+    return res;
+  }
+
+  inline bool sameId(
+      const pulseseq::EMRadiation &emr1,
+      const pulseseq::EMRadiation &emr2)
+  {
+    return emr1.getId() == emr2.getId();
+  }
   
 } // namespace dnpsoup
 
