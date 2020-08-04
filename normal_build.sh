7abf11dba2360e879721a579a713ee9e945b8744
Chen Yang <yangcnju@gmail.com>
2020-08-04 16:57:06 -0400

    add RelWithDebInfo BUILD_TYPE

--- normal_build.sh
+++ normal_build.sh
@@ -7,9 +7,14 @@ cd build
 #EXTRA_FLAGS="-DDNPSOUP_VERBOSE=1"
 EXPTRA_FLAGS=""
 
-BUILD_TYPE=Release
+#BUILD_TYPE=Release
+BUILD_TYPE=RelWithDebInfo
 CC=clang++
 C=clang
+#CC=c++
+#C=gcc
+
+
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_COMPILER=$C -DCMAKE_CXX_COMPILER=$CC -GNinja .. $EXTRA_FLAGS
 
 ninja
