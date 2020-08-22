9381306b9ba1b1a1b3cfef563f18583fd79f77ce
Chen Yang <yangcnju@gmail.com>
2020-08-21 22:12:44 -0400

    update CE results

--- quick_run.sh
+++ quick_run.sh
@@ -16,6 +16,30 @@ VERSION=
 
 APPENDIX=""
 
+FILENAME=eeH_test_coord_e2_a0b60g150_H_xn2y2z1_p1ms_a0b0g0_9p393T_inc40ns_buildup
+./take_one_input.sh $FILENAME $CATEGORYNAME $VERSION
+echo ""
+FILENAME=eeH_test_coord_e2_a0b60g150_H_xn2y2z1_p1ms_a0b15g0_9p393T_inc40ns_buildup
+./take_one_input.sh $FILENAME $CATEGORYNAME $VERSION
+echo ""
+FILENAME=eeH_test_coord_e2_a0b60g150_H_xn2y2z1_p1ms_a0b30g0_9p393T_inc40ns_buildup
+./take_one_input.sh $FILENAME $CATEGORYNAME $VERSION
+echo ""
+FILENAME=eeH_test_coord_e2_a0b60g150_H_xn2y2z1_p1ms_a0b45g0_9p393T_inc40ns_buildup
+./take_one_input.sh $FILENAME $CATEGORYNAME $VERSION
+echo ""
+FILENAME=eeH_test_coord_e2_a0b60g150_H_xn2y2z1_p1ms_a0b60g0_9p393T_inc40ns_buildup
+./take_one_input.sh $FILENAME $CATEGORYNAME $VERSION
+echo ""
+FILENAME=eeH_test_coord_e2_a0b60g150_H_xn2y2z1_p1ms_a0b75g0_9p393T_inc40ns_buildup
+./take_one_input.sh $FILENAME $CATEGORYNAME $VERSION
+echo ""
+FILENAME=eeH_test_coord_e2_a0b60g150_H_xn2y2z1_p1ms_a0b90g0_9p393T_inc40ns_buildup
+./take_one_input.sh $FILENAME $CATEGORYNAME $VERSION
+echo ""
+FILENAME=eeH_test_coord_e2_a0b60g150_H_xn2y2z1_p1ms_a0b105g0_9p393T_inc40ns_buildup
+./take_one_input.sh $FILENAME $CATEGORYNAME $VERSION
+echo ""
 FILENAME=eeH_test_coord_e2_a0b60g150_H_xn2y2z1_p1ms_a0b120g0_9p393T_inc40ns_buildup
 ./take_one_input.sh $FILENAME $CATEGORYNAME $VERSION
 echo ""
