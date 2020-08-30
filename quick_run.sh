a0d5a8be3ef71ac03db8759bb8ea4d822ade67b0
Chen Yang <yangcnju@gmail.com>
2020-08-30 06:30:28 -0400

    sync CE studies

--- quick_run.sh
+++ quick_run.sh
@@ -7,7 +7,7 @@ export OMP_NUM_THREADS=1
 
 #CATEGORYNAME=Top-DNP
 #CATEGORYNAME=ISE_SSE
-CATEGORYNAME=CE
+CATEGORYNAME=CE/Hu_JCP_2008/H_orientations/case4
 #CATEGORYNAME=CE/buildup_scan1ds
 #CATEGORYNAME=CE/buildup_powder_options
 #CATEGORYNAME=CE/CE_SE_fp_comparison
@@ -19,22 +19,22 @@ VERSION=
 
 APPENDIX=""
 
-#INPUTDIR=dnpsoup_analytics/inputs
-#echo $CATEGORYNAME
-#for filename in $INPUTDIR/$CATEGORYNAME/*.json; do
-#  FULLNAME=$(basename $filename)
-#  FILENAME=${FULLNAME%.json}
-#  echo $FILENAME
-#  ./take_one_input.sh $FILENAME $CATEGORYNAME $VERSION
-#  echo ""
-#done
+INPUTDIR=dnpsoup_analytics/inputs
+echo $CATEGORYNAME
+for filename in $INPUTDIR/$CATEGORYNAME/*.json; do
+  FULLNAME=$(basename $filename)
+  FILENAME=${FULLNAME%.json}
+  echo $FILENAME
+  ./take_one_input.sh $FILENAME $CATEGORYNAME $VERSION
+  echo ""
+done
 
-FILENAME=eeH_e2_a0b60g150_H_xn2y2z1_p6s_zcw144_9p393T_inc40ns_fp
-./take_one_input.sh $FILENAME $CATEGORYNAME $VERSION
-echo ""
+#FILENAME=eeH_p3s_mas8kHz_inc40ns_zcw144_fp
+#./take_one_input.sh $FILENAME $CATEGORYNAME $VERSION
+#echo ""
 
-# OUTPUTDIR=dnpsoup_analytics/outputs
-# OUTPUTPATH=$OUTPUTDIR/$CATEGORYNAME/$FILENAME.result$VERSION$APPENDIX
-# echo "./plot_results.py $OUTPUTPATH"
-# ./plot_results.py $OUTPUTPATH
+#OUTPUTDIR=dnpsoup_analytics/outputs
+#OUTPUTPATH=$OUTPUTDIR/$CATEGORYNAME/$FILENAME.result$VERSION$APPENDIX
+#echo "./plot_results.py $OUTPUTPATH"
+#./plot_results.py $OUTPUTPATH
 
